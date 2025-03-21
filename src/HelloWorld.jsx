eefa0642ba235d5e87edcfefbf365bd3f2b81ecd
Swingster <150227275+Swingster@users.noreply.github.com>
2025-03-20 22:39:46 -0400

    Addition of the employee list on a table form
    added the employee components to display the list of employees in the database as well as add a header and footer

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <h1 className="text-center">
+        HelloWorld!
+    </h1>
+  )
+}
+
+export default HelloWorld
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">Copyright © 2021 Employee Management System</span>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
